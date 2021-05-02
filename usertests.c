b66345f153c081f24d550755d0416712e02c15d4
Adarsh Ninganur <adarsh.ninganur@gmail.com>
2021-05-02 22:58:08 +0530

    final

--- usertests.c
+++ usertests.c
@@ -1878,72 +1878,6 @@ void sig_pause(){
   }
 }
 
-void 
-cont_handler(int signal)
-{
-  __asm__ ("movl $0x0,%eax\n\t");
-  __asm__ ("movl $0x43,%ebx\n\t");
-  __asm__ ("movl $0x76,%ecx\n\t");
-}
-
-void 
-tstp_handler(int signal)
-{
-  __asm__ ("movl $0x25,%eax\n\t");
-  __asm__ ("movl $0x87,%ebx\n\t");
-  __asm__ ("movl $0x06,%ecx\n\t");
-}
-
-void
-signaltest()
-{
-  printf (1, "signaltest\n");
-  int i = 0;
-  int pid = fork();
-  
-   //child process
-  if(pid == 0) {
-    signal(SIGCONT, cont_handler);
-    signal(SIGTSTP, tstp_handler);
-    register uint eax asm ("%eax");
-    register uint ebx asm ("%ebx");
-    register uint ecx asm ("%ecx");
-    __asm__ ("movl $0x21,%eax\n\t");
-    __asm__ ("movl $0x10,%ebx\n\t");
-    __asm__ ("movl $0x03,%ecx\n\t");
-    uint saved_eax = eax;
-    uint saved_ebx = ebx;
-    uint saved_ecx = ecx;
-    for(i = 0; i < 600; i++){
-      //printf (1, "eax %d ebx %d ecx %d\n", eax, ebx, ecx);
-      saved_eax = eax;
-      saved_ebx = ebx;
-      saved_ecx = ecx;
-      if (saved_eax != 33 || saved_ebx != 16 || saved_ecx != 3){
-        break;
-      } 
-      else{ 
-        sleep(1);
-        continue;
-      }
-    }
-    if (i == 600){
-      printf (1, "signaltest OK\n");
-    }
-    else{
-      printf (1, "signaltest failed\n");
-    }
-  } 
-
-//parent process
-  else
-  {
-    sleep(200);
-    sendkill(pid, SIGCONT);
-  
-    wait();
-  }
-}
 
 unsigned long randstate = 1;
 unsigned int
@@ -2006,8 +1940,6 @@ main(int argc, char *argv[])
   iref();
   forktest();
   bigdir(); // slow
-  //signal 
-  signaltest();
 
   uio();
 
